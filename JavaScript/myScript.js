0be76c59e9206e51839a07971cd1d146b6e352e8
Ben <benbackman54@gmail.com>
2019-10-11 11:34:10 +0300

    asd

--- JavaScript/myScript.js
+++ JavaScript/myScript.js
@@ -98,36 +98,36 @@ $(document).ready(function(){
     $('#project1').click(function(){
         $('#dd1,#project1-overlay').toggleClass('active');
         $('#project1-name').fadeToggle();
-       
+
         $('#dd2,#dd3,#dd4,#dd5,#dd6,#project2-overlay,#project3-overlay,#project4-overlay,#project5-overlay,#project6-overlay').removeClass('active');
         $('#project2-name,#project3-name,#project4-name,#project5-name,#project6-name').fadeIn();
 
-//                ------------------------      content animation   
+    //                ------------------------      content animation   
         $('#project1-open').fadeToggle('fast');
         $('#type-text1').delay(200).animate({
             left: '0',
             opacity: '1'
         },600);
-        
+
         $('#project2-open,#project3-open,#project4-open,#project5-open,#project6-open').fadeOut('fast');
-        
+
     })
     
     
     $('#project2').click(function(){
         $('#dd2,#project2-overlay').toggleClass('active');
         $('#project2-name').fadeToggle();
-        
+
         $('#dd1,#dd3,#dd4,#dd5,#dd6,#project1-overlay,#project3-overlay,#project4-overlay,#project5-overlay,#project6-overlay').removeClass('active');
         $('#project1-name,#project3-name,#project4-name,#project5-name,#project6-name').fadeIn();
 
-//                ------------------------     content animation    
+    //                ------------------------     content animation    
         $('#project2-open').fadeToggle('fast');
         $('#type-text2').delay(200).animate({
             left: '0',
             opacity: '1'
         },600);
-        
+
         $('#project1-open,#project3-open,#project4-open,#project5-open,#project6-open').fadeOut('fast');
     })
     
@@ -135,17 +135,17 @@ $(document).ready(function(){
     $('#project3').click(function(){
         $('#dd3,#project3-overlay').toggleClass('active');
         $('#project3-name').fadeToggle();
-//                ------------------------
+    //                ------------------------
         $('#dd2,#dd1,#dd4,#dd5,#dd6,#project2-overlay,#project1-overlay,#project4-overlay,#project5-overlay,#project6-overlay').removeClass('active');
         $('#project2-name,#project1-name,#project4-name,#project5-name,#project6-name').fadeIn();
 
-//                ------------------------      content animation   
+    //                ------------------------      content animation   
         $('#project3-open').fadeToggle('fast');
         $('#type-text3').delay(200).animate({
             left: '0',
             opacity: '1'
         },600);
-        
+
         $('#project2-open,#project1-open,#project4-open,#project5-open,#project6-open').fadeOut('fast');
 
     })
@@ -154,17 +154,17 @@ $(document).ready(function(){
     $('#project4').click(function(){
         $('#dd4,#project4-overlay').toggleClass('active');
         $('#project4-name').fadeToggle();
-//                ------------------------
+    //                ------------------------
         $('#dd2,#dd3,#dd1,#dd5,#dd6,#project2-overlay,#project3-overlay,#project1-overlay,#project5-overlay,#project6-overlay').removeClass('active');
         $('#project2-name,#project3-name,#project1-name,#project5-name,#project6-name').fadeIn();
 
-//                ------------------------      content animation   
+    //                ------------------------      content animation   
         $('#project4-open').fadeToggle('fast');
         $('#type-text4').delay(200).animate({
             left: '0',
             opacity: '1'
         },600);
-        
+
         $('#project2-open,#project3-open,#project1-open,#project5-open,#project6-open').fadeOut('fast');
 
     })
@@ -173,17 +173,17 @@ $(document).ready(function(){
     $('#project5').click(function(){
         $('#dd5,#project5-overlay').toggleClass('active');
         $('#project5-name').fadeToggle();
-//                ------------------------
+    //                ------------------------
         $('#dd2,#dd3,#dd4,#dd1,#dd6,#project2-overlay,#project3-overlay,#project4-overlay,#project1-overlay,#project6-overlay').removeClass('active');
         $('#project2-name,#project3-name,#project4-name,#project1-name,#project6-name').fadeIn();
 
-//                ------------------------      content animation   
+    //                ------------------------      content animation   
         $('#project5-open').fadeToggle('fast');
         $('#type-text5').delay(200).animate({
             left: '0',
             opacity: '1'
         },600);
-        
+
         $('#project2-open,#project3-open,#project4-open,#project1-open,#project6-open').fadeOut('fast');
 
     })
@@ -192,17 +192,17 @@ $(document).ready(function(){
     $('#project6').click(function(){
         $('#dd6,#project6-overlay').toggleClass('active');
         $('#project6-name').fadeToggle();
-//                ------------------------
+    //                ------------------------
         $('#dd2,#dd3,#dd4,#dd5,#dd1,#project2-overlay,#project3-overlay,#project4-overlay,#project5-overlay,#project1-overlay').removeClass('active');
         $('#project2-name,#project3-name,#project4-name,#project5-name,#project1-name').fadeIn();
 
-//                ------------------------      content animation   
+    //                ------------------------      content animation   
         $('#project6-open').fadeToggle('fast');
         $('#type-text6').delay(200).animate({
             left: '0',
             opacity: '1'
         },600);
-        
+
         $('#project2-open,#project3-open,#project4-open,#project5-open,#project1-open').fadeOut('fast');
 
     })
@@ -229,13 +229,16 @@ $(document).ready(function(){
     
     
     function frameLooper(id, targetNumber, totalSeconds, currentNumber) {
- currentNumber = currentNumber + targetNumber / (totalSeconds * 60)
- if (currentNumber > targetNumber) currentNumber = targetNumber
- if (Number(document.getElementById(id).firstChild.textContent) < targetNumber) {
-   document.getElementById(id).firstChild.textContent = Math.round(currentNumber)
-   requestAnimationFrame(() => frameLooper(id, targetNumber, totalSeconds, currentNumber));
- }
-}
+        
+        currentNumber = currentNumber + targetNumber / (totalSeconds * 60)
+        
+        if (currentNumber > targetNumber) currentNumber = targetNumber
+        
+        if (Number(document.getElementById(id).firstChild.textContent) < targetNumber) {
+            document.getElementById(id).firstChild.textContent = Math.round(currentNumber)
+            requestAnimationFrame(() => frameLooper(id, targetNumber, totalSeconds, currentNumber));
+        }
+    }
     
     
     var $awards = $('.awards-received');
